bc2fc21f52709f59943b89619ba5ba81fbdd0de5
yelyzart <yelyzart@gmail.com>
2022-09-07 13:36:55 +0300

    add Meanings.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,7 +5,7 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-  let [results, setResults] = useState({});
+  let [results, setResults] = useState(null);
 
   function whatKeyword(event) {
     setKeyword(event.target.value);
