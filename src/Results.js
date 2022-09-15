8b8e3b92efdf7b61cea1e0159c058de464838083
yelyzart <yelyzart@gmail.com>
2022-09-15 14:33:30 +0300

    fix ;

--- src/Results.js
+++ src/Results.js
@@ -20,7 +20,7 @@ export default function Results(props) {
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
-              <Meaning meaning={meaning} />;
+              <Meaning meaning={meaning} />
             </div>
           );
         })}
